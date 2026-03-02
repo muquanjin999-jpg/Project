f670b5473c80627009b6b721759056385855e262
muquanjin999-jpg <muquanjin999@gmail.com>
2026-03-02 18:48:42 +0000

    Delete card directory

--- card/IdGenerator.java
+++
@@ -1,9 +0,0 @@
-package game.card;
-
-public class IdGenerator {
-    private int counter = 1;
-
-    public synchronized String next(String prefix) {
-        return prefix + (counter++);
-    }
-}
--- card/effect/Effect.java
+++
@@ -1,8 +0,0 @@
-package game.card.effect;
-
-import game.card.CardPlayContext;
-import game.card.CardTarget;
-
-public interface Effect {
-    void apply(CardPlayContext ctx, String casterPlayerId, CardTarget target);
-}
